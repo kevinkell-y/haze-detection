257674c061cc2a31214773f9ae5b2640041ed5bd
Kevin Kelly <kelly1@protonmail.com>
2022-05-19 11:17:58 -0700

    Function: Make framelets from RAW image file

---
+++ create_framelets.py
@@ -0,0 +1,22 @@
+#=================================================================
+#
+# Divides a raw image into its indivudal framelets
+# Modularized component from NASA JPL's JunoCamImageProcessing_v2.py script
+#
+#=================================================================
+from PIL import Image
+
+def divide_image(image):
+    width, height = image.size
+    length = int(height/128)
+    image_list = []
+    ref_pixels = image.load()
+    for i in range(length):
+        img = Image.new("RGB", (width, 128))
+        pixels = img.load()
+        for w in range(width):
+            for h in range(128):
+                val = ref_pixels[w, i * 128 + h]
+                pixels[w, h] = val
+        image_list.append(img)
+    return image_list
